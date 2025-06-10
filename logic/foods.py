8321ee7a1230fd177f6d4e9c3be0b02b550f4c29
kenny507 <kennytran1234@yahoo.com>
2025-06-10 17:04:35 -0400

    Added Pork katsu instructions and modified eggs

--- logic/foods.py
+++ logic/foods.py
@@ -15,11 +15,11 @@ BOILING_IMAGES = {
 }
 
 BOILING_INSTRUCTIONS = {
-    "Sticky Rice": "Place bamboo leaf-wrapped rice in boiling water for 15 minutes.",
-    "Braised Pork": "Brown pork. Simmer with soy sauce, sugar, wine, and water for 45 minutes.",
-    "Boiled Eggs": "Start in cold water. When boiling, time for 7 minutes, then ice bath.",
-    "Instant Noodles": "Add flavor packets and block to boiling water. Boil for 2.5 mins.",
-    "Pasta Noodles": "Boil water with salt. Add noodles and cook for 10 minutes."
+    "Sticky Rice": "Place bamboo leaf-wrapped rice in boiling water for 15 minutes. Enjoy!",
+    "Braised Pork": "Brown pork. Simmer with soy sauce, sugar, wine, and water for 45 minutes. Enjoy!",
+    "Boiled Eggs": "Start in cold water. When boiling, time for 7 minutes, then ice bath. Enjoy!",
+    "Instant Noodles": "Add flavor packets and block to boiling water. Boil for 2.5 mins. Enjoy!",
+    "Pasta Noodles": "Boil water with salt. Add noodles and cook for 10 minutes. Enjoy!"
 }
 
 FRYING_TIMES = {
@@ -39,9 +39,9 @@ FRYING_IMAGES = {
 }
 
 FRYING_INSTRUCTIONS = {
-    "Eggs Sunny Side": "Heat pan with oil. Crack egg gently. Fry for 3 minutes.",
+    "Eggs Sunny Side": "Heat pan with oil. Crack egg gently. Fry for 3 minutes to obtain golden brown crust on the bottom. Enjoy!",
     "Bacon": "Lay strips on cold pan. Turn heat to medium. Flip until crisp, ~5 mins.",
     "Fried Rice": "Heat oil, fry onion and garlic. Add rice and soy sauce. Stir-fry 10 mins.",
-    "Pork Chops": "Season chops. Sear both sides in oil for ~4 mins each.",
+    "Pork Katsu": "After seasoning the pork and bread crumbs, add a small layer of oil at bottom of frying pan. After, put pork katsu into oil to fry for 3 minutes on each side with a oil temperature of 350 degrees. Enjoy! ",
     "Fish": "After adding oil to pan, wait till temperature reaches 375 degrees and add fish on skin side first. Afterwards, flip at 2.5 minutes and allow remaining minutes on the other side. Enjoy!"
 }
